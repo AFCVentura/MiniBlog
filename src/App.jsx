ec3cc227dd52cc4dd6538e3b3abab151e7597173
Ventura <ventura.py3@gmail.com>
2024-10-02 11:35:24 -0300

    feat: Implements basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
         <div className="container">
           <Routes>
-            
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
