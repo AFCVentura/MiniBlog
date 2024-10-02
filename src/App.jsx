c6788b2f50a2ebc07b8322560fbbaa78dfd667a2
Ventura <ventura.py3@gmail.com>
2024-10-02 11:31:51 -0300

    feat: Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
